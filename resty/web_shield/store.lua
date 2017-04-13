92c114346514deb0778625f72df033e5a6ddb4c8
xiejiangzhi <xiejiangzhi@xiejiangzhis-iMac.local>
2017-04-13 15:44:53 +0800

    Fix redis connection
    由于 Nginx 并发性，不能缓存 redis connection

--- resty/web_shield/store.lua
+++ resty/web_shield/store.lua
@@ -39,20 +39,15 @@ end
 --
 
 function M.init_redis(host, port)
-  local cache_key = table.concat({'redis', host, port}, '-')
-  local redis = ngx.ctx[cache_key]
-  if redis then return redis end
-
-  redis = Redis:new()
+  local redis = Redis:new()
   redis:set_timeout(100)
   Logger.debug('Redis connect ' .. host .. ':' .. port)
-  ok, err = redis:connect(host, port)
+  local ok, err = redis:connect(host, port)
 
   if ok then
-    ngx.ctx[cache_key] = redis
     return redis
   else
-    ngx.log(ngx.ERR, err)
+    Logger.err(err)
     return nil, err
   end
 end
